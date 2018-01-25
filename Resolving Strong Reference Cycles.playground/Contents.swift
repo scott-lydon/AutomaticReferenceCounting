357587a1aa4bb7c5491342b84dff28b1c36f7b2f
scott-lydon <scottlydon18@gmail.com>
2018-01-24 22:25:27 -0800

    Resolving Strong reference cycles.

--- Resolving Strong Reference Cycles.playground/Contents.swift
+++ Resolving Strong Reference Cycles.playground/Contents.swift
@@ -4,6 +4,7 @@ class HTMLElement {
     let name: String
     let text: String?
     lazy var asHTML: () -> String  = {
+        [unowned self] in
         return "<\(self.name)>\(self.text ?? "")</\(self.name)>"
     }
     
@@ -16,4 +17,7 @@ class HTMLElement {
 
 var headerTitle: HTMLElement? = HTMLElement(name: "h1", text: "Welcome")
 print(headerTitle!.asHTML())
+print(#line)
 headerTitle = nil
+print(#line)
+
